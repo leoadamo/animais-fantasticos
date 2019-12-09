4c1b99ac435c46ac3262efef5441e675d6bea2ef
Leonardo Adamoli <leonardo.adamoli@prodigious.com>
2019-12-09 16:36:34 -0300

    navegacao por tab, accordion e scroll suave para links internos

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ function initTabNav() {
 	const tabContent = document.querySelectorAll('.js-tabcontent-tracker section');
 
 	if (tabs.length && tabContent.length) {
-		tabContent[0].classList.add('ativo');
+		tabContent[0].classList.add('active');
 
 		function activeTab(index) {
 			tabContent.forEach(section => {
-				section.classList.remove('ativo');
+				section.classList.remove('active');
 			});
-			tabContent[index].classList.add('ativo');
+			tabContent[index].classList.add('active');
 		}
 
 		tabs.forEach((tab, index) => {
@@ -25,10 +25,34 @@ const accordionList = document.querySelectorAll('.js-accordion-tracker dt');
 
 function activeAccordion() {
 	this.classList.toggle('active');
-	//this.children[0].classList.toggle('active');
-	this.nextElementSibling.classList.toggle('ativo');
+	this.nextElementSibling.classList.toggle('active');
 }
 
 accordionList.forEach(item => {
 	item.addEventListener('click', activeAccordion);
+});
+
+const menuLinks = document.querySelectorAll('.js-menu-tracker a[href^="#"]');
+
+function scrollToSection(e) {
+	e.preventDefault();
+
+	const href = event.currentTarget.getAttribute('href');
+	const target = document.querySelector(href);
+
+	target.scrollIntoView({
+		behavior: 'smooth',
+		block: 'start'
+	});
+
+	/* forma alternativa, porém ainda não muito bem suportada
+	const offsetTop = target.offsetTop;
+	window.scrollTo({
+		top: offsetTop,
+		behavior: 'smooth'
+	}); */
+}
+
+menuLinks.forEach(link => {
+	link.addEventListener('click', scrollToSection);
 });
